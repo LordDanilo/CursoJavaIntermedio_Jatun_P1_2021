8fbc7d2090b3bfa49c1affaccbac982827acaa0b
LordDanilo <64174678+LordDanilo@users.noreply.github.com>
2021-10-16 11:02:05 -0400

    EDL

--- EjemplosEstructurasLineales/EjemploLista.java
+++ EjemplosEstructurasLineales/EjemploLista.java
@@ -14,4 +14,25 @@ public class EjemploLista{
     public void agregarDatoLista(int dato){
         lista.add(dato);
     }
+    public String funcion(LinkedList<Integer> lista){
+        String res = "";
+        for(int i=0; i<lista.size(); i++){
+            int act = lista.get(i);
+            if(act%2==0){
+                res = res+act+" ";
+            }
+        }
+        return res;
+    }
+    public static void main(String args[]){
+        EjemploLista var1 = new EjemploLista();
+        LinkedList<Integer> lista = new LinkedList<>();
+        lista.add(3);
+        lista.add(23);
+        lista.add(40);
+        lista.add(22);
+        lista.add(14);
+        String res = var1.funcion(lista);
+        System.out.print(res);
+    }
 }
